a2128b66850645e0b0ee3699f02fe5407ab28a49
jans027 <jans027@gmail.com>
2022-07-29 15:14:16 -0500

    section

---
+++ src/styles/Section.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+
+
+export const Section = styled.section`
+        display: flex;
+        flex-direction:column;
+        justify-content: center;
+        padding:20vh 0 0 0;
+        height:100fr;
+        width: max(43ch, 80%);
+        margin: 5em auto;
+        border-radius: 1.2em;
+        border: 1px solid aquamarine;
+`
+
+export const Construction = styled.div`
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        img{
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                object-position: center center;
+        }
+`
+
+
+
+
+
+
+
+
+
